e63af59ae1288dafe1fb09c4e714e3537b541901
Magistry <Magistrigusti@yandex.ru>
2023-04-15 06:59:08 +0300

    0.3

---
+++ src/components/List/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import classNames from 'classnames';
+import './list.scss';
+
+const List = ({ items, isRemovable }) => {
+  return (
+    <ul className="list">
+      {
+        items.map((item, index) => <li key={index}
+          className={classNames(item.className, {active: item.active})}>
+          <i>{item.icon ? item.icon : 
+          <i className={`badge badge--${item.color}`}></i> }</i>
+          <span>{item.name}</span>
+        </li>)
+      }
+    </ul>
+  )
+}
+
+export default List;
