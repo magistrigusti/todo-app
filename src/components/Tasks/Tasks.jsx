01cd68d49ad8bf836a177bfe3f8770fe461ae478
Magistry <Magistrigusti@yandex.ru>
2023-05-17 21:09:14 +0300

    1.1

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import editSvg from '../../assets/img/edit.svg';
-import './Tasks.scss'; 
+import './Tasks.scss';
 
 const Tasks = (props) => {
   return (
@@ -11,10 +11,22 @@ const Tasks = (props) => {
       </h2>
 
       <div className="tasks__items">
-        <div className="checkbox">
+        <div className="tasks__items-row">
+          <div className="checkbox">
           <input id="check" type="checkbox" />
-          <label htmlFor="check"></label>
+          <label htmlFor="check">
+            <svg width="11" height="8" viewBox="0 0 11 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001" stroke="#b2b2b2" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+          </label>
+          </div>
+          <input value="ReactJS Hooks (useState, useReducer, useEffect)" />
+          
         </div>
+        
+      </div>
+
+      <div>
       </div>
     </div>
   )
