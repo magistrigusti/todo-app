5b7dacafdcb5057b99732368ca89685e10eff760
Magistry <Magistrigusti@yandex.ru>
2023-05-17 15:25:35 +0300

    1.09

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import penSvg from '../../assets/img/pen.svg';
+import editSvg from '../../assets/img/edit.svg';
 import './Tasks.scss'; 
 
 const Tasks = (props) => {
   return (
     <div className="tasks">
       <h2 className="tasks__title">
-        
+        Frontend
+        <img src={editSvg} alt="Edit icon" />
       </h2>
+
+      <div className="tasks__items">
+        <div className="checkbox">
+          <input id="check" type="checkbox" />
+          <label htmlFor="check"></label>
+        </div>
+      </div>
     </div>
   )
 }
